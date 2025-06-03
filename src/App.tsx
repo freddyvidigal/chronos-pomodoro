c7d11a5f2f5f0f117530dc630ce3588c82b9271d
freddyvidigal <freddyvidigalads@gmail.com>
2025-06-03 12:11:20 -0300

    Add CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,36 @@ import { Logo } from './components/Logo';
 import './Styles/theme.css';
 import './Styles/global.css';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 export function App() {
   return (
     <>
       <Container>
-        <Heading>
-          <Logo />
-        </Heading>
+        <Logo />
       </Container>
       <Container>
-        <Heading>
-          <Menu />
-        </Heading>
+        <Menu />
       </Container>
       <Container>
-        <section>FORM</section>
+        <CountDown />
+      </Container>
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <label htmlFor='meuInput'>task</label>
+            <input id='meuInput' type='text' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit ame</p>
+          </div>
+          <div className='formRow'>
+            <p>ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>enviar</button>
+          </div>
+        </form>
       </Container>
       <Container>
         <section>FOOTER</section>
