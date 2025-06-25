2d2b4fc9ad441c8d855225d928a9641b97042a7e
freddyvidigal <freddyvidigalads@gmail.com>
2025-06-25 19:06:16 -0300

    començando a configurar

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './taskContext';
 
@@ -7,6 +7,11 @@ type TaskContextProviderProps = {
 };
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
   return (
     <TaskContext.Provider value={{ state, setState }}>
       {children}
