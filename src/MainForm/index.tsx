0793260eab07a953ca30878f19ff35841974299b
freddyvidigal <freddyvidigalads@gmail.com>
2025-07-03 15:47:10 -0300

    implementado tips

--- src/MainForm/index.tsx
+++ src/MainForm/index.tsx
@@ -7,10 +7,12 @@ import type { TaskModel } from '../models/TaskModel';
 import { useTaskContext } from '../contexts/TaskContext/useTaskContext';
 import { getNextCycle } from '../utils/getNextCycle';
 import { getNextCycleType } from '../utils/getNextCycleType';
-import { formatSecondsToMinutes } from '../utils/formatSecondsToMinutes';
+
+import { TaskActionTypes } from '../contexts/TaskContext/taskActions';
+import { Tips } from '../components/Tips';
 
 export function MainForm() {
-  const { state, setState } = useTaskContext();
+  const { state, dispatch } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
   //ciclos
   const nextCycle = getNextCycle(state.currentCycle);
@@ -33,34 +35,11 @@ export function MainForm() {
       duration: state.config[nextCycleType],
       type: nextCycleType,
     };
-    const secondsRemaining = newTask.duration * 60;
-    setState(prevState => {
-      return {
-        ...prevState,
-        config: { ...prevState.config },
-        activeTask: newTask,
-        currentCycle: nextCycle,
-        secondsRemaining, // conferir
-        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
-        tasks: [...prevState.tasks, newTask],
-      };
-    });
+
+    dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
   }
   function handleInterruptTask() {
-    setState(prevState => {
-      return {
-        ...prevState,
-        activeTask: null,
-        secondsRemaining: 0,
-        formattedSecondsRemaining: '00:00',
-        tasks: prevState.tasks.map(task => {
-          if (prevState.activeTask && prevState.activeTask.id === task.id) {
-            return { ...task, interruptDate: Date.now() };
-          }
-          return task;
-        }),
-      };
-    });
+    dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 
   return (
@@ -71,12 +50,12 @@ export function MainForm() {
           id='meuInput'
           type='text'
           placeholder='Digite algo'
-          //
           ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
       <div className='formRow'>
-        <p>Próximo intervalo é de 25 minutos</p>
+        <Tips />
       </div>
       {state.currentCycle > 0 && (
         <div className='formRow'>
